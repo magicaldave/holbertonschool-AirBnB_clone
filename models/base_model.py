9ae8b3027c257dc800279918bd22ed6fec40c00e
Dave Corley <s3kshun-8@pop-os.localdomain>
2022-09-28 23:09:45 -0500

    Add: gitignore, base_model.py with __str__, save, and to_dict methods.

---
+++ models/base_model.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Base model for AirBnB Project
+"""
+
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    """Main Class Body"""
+
+    def __init__(self, name="Test"):
+        """ Initializer Function """
+        self.name = name
+        self.id = str(uuid4())
+        self.updated_at = self.created_at = datetime.now()
+
+    def __str__(self):
+        """
+        Prints string representation of the data object
+        """
+        return f"[{self.name}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """
+        For now, simply updates date of the object.
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        Returns a dictionary with all object data.
+        Not complete. Needs to refine the updated/created_at values
+        and add the __class__ key.
+        The --outdict-- variable is where the relevant data is
+        stored, transformed, and returned.
+        """
+        return dict(self.__dict__,
+                    __class__=self.__class__.__name__,
+                    updated_at=self.updated_at.isoformat(),
+                    created_at=self.created_at.isoformat())
