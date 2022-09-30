cfcd1bb0c566149d8a1ca727351ebf6580ab388a
Clayton Christian <103296303+claybowl@users.noreply.github.com>
2022-09-29 22:54:57 -0500

    console

--- tests/test_models/console.py
+++ tests/test_models/console.py
@@ -13,7 +13,18 @@ class HBNBCommand(cmd.Cmd):
 
     prompt = "(hbnb)"
 
-    
+    def do_quit(self, line):
+        """exits program"""
+        return True
+
+    def do_EOF(self, line):
+        """handles end of line char"""
+        print()
+        return True
+
+    def emptyline(self):
+        """does nothing on enter"""
+        pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
