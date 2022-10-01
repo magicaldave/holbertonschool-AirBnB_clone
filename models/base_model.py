14379db3a8195cc5e7931da4d7e06d10cff4c0af
Dave Corley <s3kshun-8@pop-os.localdomain>
2022-09-30 19:54:37 -0500

    Edit: re-added args

--- models/base_model.py
+++ models/base_model.py
@@ -11,7 +11,7 @@ from .__init__ import storage
 class BaseModel:
     """Main Class Body"""
 
-    def __init__(self, **kwargs):
+    def __init__(self, *args, **kwargs):
         """ Initializer Function """
 
         if kwargs:
