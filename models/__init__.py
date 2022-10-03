4725ac9cf20eb62e9f2cd1e37d327d1d62ab8910
Clayton Christian <103296303+claybowl@users.noreply.github.com>
2022-10-03 13:34:48 -0500

    added import

--- models/__init__.py
+++ models/__init__.py
@@ -3,6 +3,8 @@
 Initializes an instance of the data storage class.
 """
 from .engine.file_storage import FileStorage
+from models import base_model
+from models.base_model import BaseModel
 
 storage = FileStorage()
 storage.reload()
