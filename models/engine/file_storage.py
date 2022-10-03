c56694f1d5f6885cb62321263ead7ed9f159be1f
magicaldave <smokydave@icloud.com>
2022-10-03 12:55:00 -0500

    Edit: imported BaseModel to init, BaseModel now translates ISO format string back to previous format with strptime in init. Some calls to storage.self changed to models.storage.self. Reload method of file_storage now properly instantiates objects instead of just grabbing a dictionary.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,6 +5,7 @@ File Storage Engine Module
 
 import json
 from os.path import exists
+import models
 
 
 class FileStorage:
@@ -45,4 +46,7 @@ class FileStorage:
         """
         if exists(self.__file_path):
             with open(self.__file_path, encoding='utf-8') as ininstances:
-                self.__objects = json.load(ininstances)
+                old_instances = json.load(ininstances)
+                for key in old_instances:
+                    self.__objects[key] =  getattr(models,
+                            old_instances[key]['__class__'])(**old_instances[key])
