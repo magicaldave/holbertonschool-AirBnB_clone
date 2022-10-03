fc7ffb334dd480ede6615ac2f47e771a6645b2e2
Clayton Christian <103296303+claybowl@users.noreply.github.com>
2022-10-03 16:15:56 -0500

    added a few imports to __init__

--- models/__init__.py
+++ models/__init__.py
@@ -5,6 +5,11 @@ Initializes an instance of the data storage class.
 from .engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 
 
 storage = FileStorage()
--- models/state.py
+++ models/state.py
@@ -7,7 +7,7 @@ Contains data for state module
 from models.base_model import BaseModel
 
 
-class State (BaseModel):
+class State(BaseModel):
     """class represnting State"""
 
     name = ""
